0378422917e1ed645f4d71f048e8d5e5418a03ff
Yousef AlShaikh <164582835+yousefalshaikh17@users.noreply.github.com>
2025-05-13 02:51:11 +0100

    Refactored main file and gameloop & updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SFML 2D Game Engine (WIP)
+
+Currently, this only renders a circle which can be moved with WASD.
--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,59 @@
 #include <memory>
 #include <vector>
 
+static float renderDeltaTime = 0;
+static float updateDeltaTime = 0;
+static float movementMultiplier = 300;
+
 static sf::RenderWindow InitWindow()
 {
     sf::ContextSettings settings;
     settings.antiAliasingLevel = 8;
     return sf::RenderWindow(sf::VideoMode({ 800, 600 }), "Game Instance", sf::State::Windowed, settings);
+}
+
+static void Update(sf::CircleShape& obj)
+{
+    // Input
+    sf::Vector2f movement(0.f, 0.f);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) movement.y -= 1;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) movement.y += 1;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) movement.x -= 1;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) movement.x += 1;
+
+    // Updates
+    obj.move(movement * updateDeltaTime * movementMultiplier);
+}
+
+static void Render(sf::RenderWindow& window, sf::CircleShape& obj)
+{
+    // The window is like a canvas
+    window.clear();
+    window.draw(obj);
+
+    // Display backbuffer
+    window.display();
+}
+
+static void GameLoop(sf::RenderWindow& window, sf::CircleShape& obj)
+{
+    sf::Clock renderClock;
+    sf::Clock updateClock;
+
+    std::optional<sf::Event> event;
+
+    while (window.isOpen())
+    {
+        while (event = window.pollEvent())
+            if (event->is<sf::Event::Closed>())
+                window.close();
+
+        updateDeltaTime = updateClock.restart().asSeconds();
+        Update(obj);
+
+        renderDeltaTime = renderClock.restart().asSeconds();
+        Render(window, obj);
+    }
 }
 
 int main()
@@ -19,41 +67,7 @@ int main()
     auto circle = sf::CircleShape(50.0f);
     circle.setFillColor(sf::Color::Green);
 
-    sf::Clock clock;
+    GameLoop(window, circle);
 
-    float deltaTime = 0;
-    float movementMultiplier = 300;
-    std::optional<sf::Event> event;
-
-    sf::CircleShape obj = circle;
-
-    while (window.isOpen())
-    {
-        deltaTime = clock.restart().asSeconds();
-        while (event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        // Input
-        sf::Vector2f movement(0.f, 0.f);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) movement.y -= 1;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) movement.y += 1;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) movement.x -= 1;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) movement.x += 1;
-
-        // Updates
-        obj.move(movement * deltaTime * movementMultiplier);
-
-        // Render
-        // The window is like a canvas
-        window.clear();
-        window.draw(obj);
-
-        // Display backbuffer
-        window.display();
-    }
-    
-
+    // Handle exit
 }
