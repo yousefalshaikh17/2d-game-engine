4db88b673af3eda48ed2af9856c63aa69b46a72a
Yousef AlShaikh <164582835+yousefalshaikh17@users.noreply.github.com>
2025-06-01 16:40:16 +0100

    Refactor `Player` to implement `RenderComponent`

--- src/GameObjects/Player.h
+++ src/GameObjects/Player.h
@@ -2,16 +2,14 @@
 
 #include <SFML/Graphics.hpp>
 #include "../GameObject.h"
+#include "../Components/RenderComponent.h"
 
 class Player : public GameObject {
 private:
-    sf::CircleShape circle;
     float movementMultiplier = 300;
     sf::Vector2f velocity;
 public:
     Player();
     virtual ~Player();
     void fixedUpdate(float deltaTime) override;
-    void preRender(float alpha) override;
-    void render(sf::RenderWindow& window) override;
 };
